8e781455f121ed4855ec776d67ff1ccdd59c1556
Phillip Mantatsky <pmantatsky1@bsd220.org>
2024-02-21 09:30:38 -0600

    Initial Commit

---
+++ Map Hackwich/Map_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_HackwichApp.swift
+//  Map Hackwich
+//
+//  Created by Phillip Mantatsky on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Map_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
